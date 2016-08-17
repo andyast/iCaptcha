96b1cdedabffc4ea07d6ec15c14ae2a2339fc932
Andy S <andyast@gmail.com>
2016-08-16 21:03:11 -0500

    initial commit

---
+++ iCaptcha/iCaptcha.h
@@ -0,0 +1,19 @@
+//
+//  iCaptcha.h
+//  iCaptcha
+//
+//  Created by Andy Steinmann on 8/16/16.
+//  Copyright © 2016 Andy Steinmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iCaptcha.
+FOUNDATION_EXPORT double iCaptchaVersionNumber;
+
+//! Project version string for iCaptcha.
+FOUNDATION_EXPORT const unsigned char iCaptchaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iCaptcha/PublicHeader.h>
+
+
